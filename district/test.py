39ed26c7c8a1c51a35beb7102e482d9cf23a54a3
zhouyixuan <easense@163.com>
2018-07-31 09:22:36 +0800

    del unrelated files

--- district/test.py
+++
@@ -1,7 +0,0 @@
-import json
-
-a = {'href': '15/07/150725.html', 'name': '陈巴尔虎旗', 'p_code': '150700000000', 'level': 3, 'code': '150725000000',
-     'type': 'county'}
-
-print(json.dumps(a, ensure_ascii=False))
-# json.dump(a, ensure_ascii=False)
